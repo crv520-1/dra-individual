8ee60538dc82574752cb1f9053997b7e0d29c034
crv520-1 <crv520@inlumine.ual.es>
2025-05-19 15:49:36 +0200

    Deseados funciona

--- Sucus/frontend/src/components/Deseados.jsx
+++ Sucus/frontend/src/components/Deseados.jsx
@@ -7,6 +7,7 @@ import './Inicio.css';
 export const Deseados = () => {
   const navigate = useNavigate();
   const [usuario, setUsuario] = useState(null);
+  const [paisesPendientes, setPaisesPendientes] = useState([]);
 
   useEffect(() => {
     const obtenerUsuario = async () => {
@@ -30,6 +31,50 @@ export const Deseados = () => {
     obtenerUsuario()
   }, [navigate]);
 
+  useEffect(() => {
+    const obtenerPaisesPendientes = async () => {
+        if (usuario) {
+            try {
+                const response = await axios.get(`http://localhost:3000/api/wishlist/user/${usuario.idUsuario}`);
+                if (response.data != null) {
+                    const paisPromises = response.data.map(async (pais) => {
+                        const responsePais = await axios.get(`http://localhost:3000/api/paises/${pais.idCountry}`);
+                        return responsePais.data.cca2;
+                    });
+
+                    const cca2Array = await Promise.all(paisPromises);
+                    const cca2String = cca2Array.join(',');
+
+                    if (cca2String) {
+                        try {
+                            const responsePaises = await axios.get(`https://restcountries.com/v3.1/alpha?codes=${cca2String}`);
+                            if (responsePaises.data != null) {
+                                setPaisesPendientes(responsePaises.data);
+                            } else {
+                                alert('Error al obtener los paises pendientes porque da null el responsePaises.data.');
+                                console.log('Error al obtener los paises pendientes porque da null el responsePaises.data.');
+                            }
+                        } catch (error) {
+                            console.error('Error al obtener los paises pendientes:', error);
+                            alert('Error al obtener los paises pendientes por alphaCodes.');
+                        }
+                    } else {
+                        setPaisesPendientes([]);
+                    }
+                } else {
+                    setPaisesPendientes([]);
+                    alert('Error al obtener los paises pendientes porque da null el response.data.');
+                    console.log('Error al obtener los paises pendientes porque da null el response.data.');
+                }
+            } catch (error) {
+                console.error('Error al obtener los paises pendientes:', error);
+                alert('Error al obtener los paises pendientes.');
+            }
+        }
+    }
+    obtenerPaisesPendientes();
+  }, [usuario]);
+
   return (
     <div className='login-container'>
         <div className='container-banner'>
@@ -44,6 +89,14 @@ export const Deseados = () => {
                     <img src={usuario.fotoPerfil} alt="Perfil" />
                 </button>
             )}
+        </div>
+        <div className='container-paises'>
+            {paisesPendientes.map((pais) => (
+                <button key={pais.cca2} className='pais-card' onClick={() => navigate('/Detalles', { state: { pais } })}>
+                    <img src={pais.flags.svg} alt={pais.translations.spa.common} className='pais-flag' />
+                    <p>{pais.capital && pais.capital[0]}, {pais.translations.spa.common}, {pais.region}</p>
+                </button>
+            ))}
         </div>
     </div>
   )
